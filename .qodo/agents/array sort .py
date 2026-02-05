e59d5d51111ade3c689cd2aa520e8420a5a7fca8
SI326 <suvasuvakovan@gmail.com>
2026-02-05 20:39:28 +0530

    reverse string

---
+++ .qodo/agents/array sort .py
@@ -0,0 +1,8 @@
+arr = [1,2,3,4,5,6]
+is_sorted = True
+for i in range(len(arr)-1):
+    if arr[i] > arr[i+1]:
+        is_sorted = False
+        break
+    print(is_sorted)
+    print(i)
---
+++ .qodo/agents/reverse an array .py
@@ -0,0 +1,7 @@
+arr = [1,2,3,4,5]
+reversed_arr =[]
+print (arr[::-1])
+print (arr[0::])
+# for i in range(len(arr),-1,-1,-1):
+#     reversed_arr.append(arr[i])
+#     print(reversed_arr)
