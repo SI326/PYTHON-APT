2c0e8c2021750e3da352cf1dc58eb7b49ce6cfa2
suvakovan <suvasuvakovan@gmail.com>
2026-02-16 16:20:44 +0530

    change

--- bubble sort.py
+++ bubble sort.py
@@ -5,3 +5,4 @@ for i in range (n):
         if arr[j] > arr[j+1]:
             arr[j] , arr[j+1] = arr[j+1] , arr[j]
 print(arr)
+print("Sorted array is:")
--- double loop.py
+++ double loop.py
@@ -2,3 +2,4 @@ arr = [1,2,3,4,5]
 for i in range (len(arr)):
     for j in range (i+1 , len(arr)):
         print(arr[i] , arr[j])
+        
