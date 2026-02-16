19aa5a20ee0902a746586752bbe9233cd3291bb7
suvakovan <suvasuvakovan@gmail.com>
2026-02-16 16:33:19 +0530

    kadane algorithm

--- kadane algorithm.py
+++ kadane algorithm.py
@@ -3,3 +3,11 @@ def max_subarray(arr):
     current_sum = 0
     for num in arr:
         current_sum += num
+        if current_sum > max_sum:
+            max_sum = current_sum
+
+        if current_sum < 0:
+            current_sum = 0
+    return max_sum
+arr =[-2,1,-4,6,-4,-1,3,5]
+print(max_subarray(arr))
